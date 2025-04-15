988c2555fc746e9569b81c3ef6c56d43a1917db6
Ranjana <ranjna.krishna@gmail.com>
2025-04-15 17:35:06 -0600

    With Session
    Pages and test

--- cypress/e2e/POMTest/ProviderDialysisFacilityTest.cy.js
+++ cypress/e2e/POMTest/ProviderDialysisFacilityTest.cy.js
@@ -7,103 +7,101 @@ import LandingPage from "../pages/LandingPage.js";
 import PharmacyPage from "../pages/PharmacyPage.js"
 import PlanselectionPage from '../pages/PlanselectionPage.js';
 
-describe('ProviderDialysisFacility Testing', () => {
+describe("ProviderDialysisFacility Testing", () => {
+    const providerDialysisFacility = new ProviderDialysisFacility();
 
-    beforeEach(() => {
-        cy.visit('http://169.61.105.110/medicareAdvantage_sandbox/medicare-advantage');
+    beforeEach("Login to PlanSelectionPage", () => {
+        cy.session("Provider session", () => {
+            cy.visit('http://169.61.105.110/medicareAdvantage_sandbox/landing-page ');
 
-        cy.fixture('LoginFixture').then((data) => {
-            const loginpage = new LoginPage();
-            loginpage.setUserName(data.username);
-            loginpage.setPassword(data.password);
-            loginpage.clickLoginBtn();
+            const lPage = new LoginPage();
+            const recPage = new LandingPage();
+            const homepage = new HomePage();
+            const preferencePage = new PreferencePage();
+            const prescriptionPage = new PrescriptionPage();
+            const pharmacyPage = new PharmacyPage();
+            const planselectionpage = new PlanselectionPage();
+
+            cy.fixture('LoginFixture').then((data) => {
+                lPage.setUserName(data.username);
+                lPage.setPassword(data.password);
+                lPage.clickLoginBtn();
+                lPage.verifyLogin();
+            });
+
+            recPage.clickCreateRecommendation();
+            cy.wait(100);
+            homepage.enterEmail("ShigaPOM@gmail.com");
+            cy.wait(100);
+            homepage.clickhealthArrow();
+            cy.wait(100);
+            homepage.clickGoodHealth();
+            cy.wait(100);
+            homepage.enterName("Shigapage");
+            cy.wait(100);
+            homepage.enterLifeexpectancy("86");
+            cy.wait(100);
+            homepage.datePickerclick();
+            cy.wait(100);
+            homepage.year1957click();
+            cy.wait(100);
+            homepage.month1957click();
+            cy.wait(100);
+            homepage.enterZip("27529")
+            cy.wait(100);
+            homepage.clickSearch();
+            cy.wait(100);
+            homepage.nextHomeClick();
+            cy.wait(100);
+            preferencePage.clickyesRadioDrugCost();
+            cy.wait(100);
+            preferencePage.clickNextPrefPage();
+            cy.wait(100);
+            prescriptionPage.enterDrugSearchBox("Gabapentin");
+            cy.wait(100);
+            prescriptionPage.selectDrug();
+            cy.wait(100);
+            prescriptionPage.clickAddToDrug();
+            cy.wait(100);
+            prescriptionPage.doneAddDrugClick();
+            cy.wait(100);
+            pharmacyPage.clickFindFarmacy();
+            cy.wait(100);
+            pharmacyPage.clickfarmacyOne();
+            cy.wait(100);
+            pharmacyPage.clickfarmacyTwo();
+            cy.wait(100);
+            pharmacyPage.clicknextpharmacy();
+            cy.wait(2000)
+            planselectionpage.setProviderButtn();
         });
-        const landingpage = new LandingPage();
-        landingpage.clickCreateRecommendation();
 
-        //Steps to go to the dialysis page
-        const homepage = new HomePage();
-        cy.wait(100);
-        homepage.enterEmail("ShigaPOM@gmail.com");
-        cy.wait(100);
-        homepage.clickhealthArrow();
-        cy.wait(100);
-        homepage.clickGoodHealth();
-        cy.wait(100);
-        homepage.enterName("Shigapage");
-        cy.wait(100);
-        homepage.enterLifeexpectancy("86");
-        cy.wait(100);
-        homepage.datePickerclick();
-        cy.wait(100);
-        homepage.year1957click();
-        cy.wait(100);
-        homepage.month1957click();
-        cy.wait(100);
-        homepage.enterZip("27529")
-        cy.wait(100);
-        homepage.clickSearch();
-        cy.wait(100);
-        homepage.nextHomeClick();
-        cy.wait(100);
-        // homepage.verifyUrl("https://analytics.dzeecloud.com/medicareAdvantage_sandbox/preferences");
+        cy.visit("http://169.61.105.110/medicareAdvantage_sandbox/manage-providers");
+    })
 
-        const prefPage = new PreferencePage();
-        cy.wait(100);
-        prefPage.clickyesRadioDrugCost();
-        cy.wait(100);
-        prefPage.clickNextPrefPage();
 
-        cy.wait(2000)
-        const drugpage = new PrescriptionPage();
-        cy.wait(100);
-        drugpage.enterDrugSearchBox("Gabapentin");
-        cy.wait(100);
-        drugpage.selectDrug();
-        cy.wait(100);
-        drugpage.clickAddToDrug();
-        cy.wait(100);
-        drugpage.doneAddDrugClick();
-        cy.wait(1000);
 
-        const Pharmacypage = new PharmacyPage();
-        cy.wait(100);
-        Pharmacypage.clickFindFarmacy();
-        cy.wait(100);
-        Pharmacypage.clickfarmacyOne();
-        cy.wait(100);
-        Pharmacypage.clickfarmacyTwo();
-        cy.wait(100);
-        Pharmacypage.clicknextpharmacy();
-        cy.wait(100);
-        //PlanselectionPage
-        const planselectionpage = new PlanselectionPage();
-        planselectionpage.setProviderButtn();
 
-        cy.log("Entering Dialysis Facility Page");
-        cy.wait(3000)
-    });
+
+    
 
     it('TC-1 dialysisfacility clickdialysis', () => {
-        const providerDialysisFacility = new ProviderDialysisFacility();
         providerDialysisFacility.clickDialysis();
 
     });
 
     it('TC-2 Enter Dialysis Facility Name', () => {
-        const providerDialysisFacility = new ProviderDialysisFacility();
+        providerDialysisFacility.clickDialysis();
         providerDialysisFacility.enterDialysisFacilityName("Davita Englewood Dialysis Center");
         cy.log("Dialysis Facility Name - Passed");
     });
 
     it('TC-3 dialysisfacility street', () => {
-        const providerDialysisFacility = new ProviderDialysisFacility();
         cy.wait(1000);
         providerDialysisFacility.enterStreet("Girard");
     });
 
     it('TC-4 dialysisfacility enterzipcode', () => {
-        const providerDialysisFacility = new ProviderDialysisFacility();
         cy.wait(1000);
         providerDialysisFacility.enterZipCode("80113");
         providerDialysisFacility.clickZipSearch();
@@ -114,13 +112,11 @@ describe('ProviderDialysisFacility Testing', () => {
     });
 
     it('TC-5 dialysisfacility clickradius', () => {
-        const providerDialysisFacility = new ProviderDialysisFacility();
         providerDialysisFacility.clickRadiusIn();
         providerDialysisFacility.clickSearch();
     });
 
     it('TC-6 dialysisfacility clickproviderfilter', () => {
-        const providerDialysisFacility = new ProviderDialysisFacility();
         //Search to get to next page.
         providerDialysisFacility.clickRadiusIn();
         providerDialysisFacility.clickSearch();
@@ -141,7 +137,6 @@ describe('ProviderDialysisFacility Testing', () => {
     });
 
     it('TC-7 dialysisfacility clickback button', () => {
-        const providerDialysisFacility = new ProviderDialysisFacility();
         //Search to get to next page.
         providerDialysisFacility.clickRadiusIn();
         cy.wait(1000);
