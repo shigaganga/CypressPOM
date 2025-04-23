f781a37c3f961a602f8c1fb01cc461b111ae2878
Rani-Yelgalwar <rani.kondpak@gmail.com>
2025-04-23 12:58:52 -0600

    Update PlanSelectionSupplementTest.cy.js
    Modified test cases no. Additional two test cases are added

--- cypress/e2e/POMTest/PlanSelectionSupplementTest.cy.js
+++ cypress/e2e/POMTest/PlanSelectionSupplementTest.cy.js
@@ -72,21 +72,38 @@ describe("Automation of test cases for PlanSelection Suppliment Page",()=>{
     cy.visit("http://169.61.105.110/medicareAdvantage_sandbox/plan-selection")
 })
 
-
-it("TC01:To verify functionality of supplement button on plan-selection/plan-list/SUPPLEMENT page and to verify that user can select max 3 plans",()=>{
+it("TC_PDP_SUPL_PLAN_42:This is to verify the 'Suppliment Button' functionality on the plan-selection page .",()=>{
     planselectionpage.setSupplementButtn()
-    
+    cy.wait(10000)
+    cy.url().should('eq','http://169.61.105.110/medicareAdvantage_sandbox/plan-selection/plan-list/SUPPLEMENT');  
+})
+
+
+it("TC_PDP_SUPL_PLAN_48:To verify the 'Select Supplement Plans' functionality on the plan-selection/plan-list/SUPPLIMENT page within the PDP application.",()=>{
+    planselectionpage.setSupplementButtn()
+    cy.wait(10000)
     planselectionpage.setPlanSelectionCheckBox(1);
     planselectionpage.setPlanSelectionCheckBox(2);
     planselectionpage.setPlanSelectionCheckBox(3);
     planselectionpage.setPlanSelectionCheckBox(4);
     
     planselectionpage.max3PlanVerifyMsg();
-    planselectionpage.setPlanSelectionCheckBox(1);  //validating the uncheck functionality of plan checkbox.
+    planselectionpage.setPlanSelectionCheckBox(1);  
 
 })
+it("TC_PDP_SUPL_PLAN_49:This is to verify the 'Deselect Supplement Plans' functionality on the plan-selection/plan-list/SUPPLIMENT page within the PDP application.",()=>{
+    planselectionpage.setSupplementButtn()
+    cy.wait(10000)
+    planselectionpage.setPlanSelectionCheckBox(1);
+    planselectionpage.setPlanSelectionCheckBox(2);
+    planselectionpage.setPlanSelectionCheckBox(3); 
+    planselectionpage.setPlanSelectionCheckBox(4);
+    
+    planselectionpage.max3PlanVerifyMsg();
+    planselectionpage.setPlanSelectionCheckBox(1); 
+})
   
-it("Test2:To verify that user can select only one supplement plan from previously selected 3 plans and able to see the plan details",()=>{
+it("TC_PDP_SUPL_PLAN_50:To verify that user can select only one supplement plan from previously selected 3 plans and able to see the plan details",()=>{
     planselectionpage.setSupplementButtn()
     cy.wait(10000)
     planselectionpage.setPlanSelectionCheckBox(1);
@@ -94,11 +111,18 @@ it("Test2:To verify that user can select only one supplement plan from previousl
     planselectionpage.setPlanSelectionCheckBox(3);
     planselectionpage.donePlanSelectionClick();
     planselectionpage.setPlanSelectionCheckBox(1);
-    planselectionpage.verifyMsg(); //asretion ,user can selectonly 1 plan out of selected 3  maximum plans.
+    planselectionpage.verifyMsg(); 
     
 })
+it("TC_PDP_SUPL_PLAN_53:To verify the functionality of Medigap plan N",()=>{
+    planselectionpage.setMedigapArrow()
+    planselectionpage.setSelectPlanN()
+   planselectionpage.setSupplementButtn();
+   cy.wait(10000)
+    cy.url().should('eq','http://169.61.105.110/medicareAdvantage_sandbox/plan-selection/plan-list/SUPPLEMENT'); 
+})
  
-it("Test3:To verify the functionality of Medigap plan N ,filter button",()=>{
+it("TC_PDP_SUPL_PLAN_56:To verify the functionality of filter icon",()=>{
     planselectionpage.setMedigapArrow()
     planselectionpage.setSelectPlanN()
    planselectionpage.setSupplementButtn();
@@ -108,18 +132,18 @@ it("Test3:To verify the functionality of Medigap plan N ,filter button",()=>{
       
 })
 
-it("Test4:To verify the functionality of Medigap plan N ,reset button",()=>{
+it("TC_PDP_SUPL_PLAN_57:To verify the functionality of reset button",()=>{
   planselectionpage.setMedigapArrow()
   planselectionpage.setSelectPlanN()
   planselectionpage.setSupplementButtn();
   cy.wait(10000)
   planselectionpage.setFilterButton() 
    planselectionpage.filterByInsuranceCarrier("HUMANA GRP (60052)")
-   planselectionpage.setResetButton(); //reset button validation
+   planselectionpage.setResetButton(); 
 
 })
 
-it("Test5:To verify the functionality of Medigap plan N details:PartA ",()=>{
+it("TC_PDP_SUPL_PLAN_62:To verify that clicking on the part A Header panel expands and display additional options. ",()=>{
     planselectionpage.setMedigapArrow()
     planselectionpage.setSelectPlanN()
     planselectionpage.setSupplementButtn();
@@ -138,7 +162,7 @@ it("Test5:To verify the functionality of Medigap plan N details:PartA ",()=>{
    planselectionpage.setPartAExpandsCollapsIndicator()
 })
  
-it("Test6:To verify the functionality of Medigap plan N details:PartA:Hospitalization",()=>{
+it("TC_PDP_SUPL_PLAN_64:Verify that clicking on the Hospitalization should expands and display additional options.",()=>{
     planselectionpage.setMedigapArrow()
     planselectionpage.setSelectPlanN()
     planselectionpage.setSupplementButtn();
@@ -147,29 +171,29 @@ it("Test6:To verify the functionality of Medigap plan N details:PartA:Hospitaliz
     planselectionpage.setPlanDetailsButton() 
     planselectionpage.setHospitalizationClick()
     cy.get(".mat-expansion-panel-body table.mat-table.cdk-table.detail-table tbody", { timeout: 10000 })
-    .should('be.visible') // Check table is visible
-   .find('tr') // Find all rows in the table
-   .should('have.length.greaterThan', 0) // Ensure at least one row exists
+    .should('be.visible') 
+   .find('tr') 
+   .should('have.length.greaterThan', 0) 
    .each(($row) => {
     cy.wrap($row).find('td').each(($cell) => {
-      cy.wrap($cell).invoke('text').should('not.be.empty'); // Ensure each cell has text
+      cy.wrap($cell).invoke('text').should('not.be.empty'); 
     });
   });
 })
 
 
-it("Test7:To verify Part B expands and collapse",()=>{
+it("TC_PDP_SUPL_PLAN_72:To verify Part B expands and collapse",()=>{
     planselectionpage.setMedigapArrow()
     planselectionpage.setSelectPlanN()
     planselectionpage.setSupplementButtn();
     cy.wait(10000)
     planselectionpage.setPlanSelectionCheckBox(1);
     planselectionpage.setPlanDetailsButton()
-    planselectionpage.setPartBExpands()   //PartB expands
-    planselectionpage.setPartBCollapse()  //Part B collapse
+    planselectionpage.setPartBExpands()   
+    planselectionpage.setPartBCollapse()  
 }) 
 
-it("Test8:To verify Part A & B expands and collapse",()=>{
+it("TC_PDP_SUPL_PLAN_80:To verify Part A & B expands and collapse",()=>{
     planselectionpage.setMedigapArrow()
     planselectionpage.setSelectPlanN()
     planselectionpage.setSupplementButtn();
@@ -180,7 +204,7 @@ it("Test8:To verify Part A & B expands and collapse",()=>{
     planselectionpage.setPartABCollapse()
 })
 
-it("Test9:To verify Other Benefits expands and collapse",()=>{
+it("TC_PDP_SUPL_PLAN_86:To verify Other Benefits expands and collapse",()=>{
     planselectionpage.setMedigapArrow()
     planselectionpage.setSelectPlanN()
     planselectionpage.setSupplementButtn();
@@ -190,19 +214,19 @@ it("Test9:To verify Other Benefits expands and collapse",()=>{
     planselectionpage.setOtherBenefitsExpands()
     planselectionpage.setOtherBenefitsCollapse()
 })
- it("Test10:To verify functionality of Back button on suppliment plan details page",()=>{
+ it("TC_PDP_SUPL_PLAN_91:To verify functionality of Back button on suppliment plan details page",()=>{
     planselectionpage.setMedigapArrow()
     planselectionpage.setSelectPlanN()
     planselectionpage.setSupplementButtn();
     cy.wait(10000)
     planselectionpage.setPlanSelectionCheckBox(1);
     planselectionpage.setPlanDetailsButton()
-    planselectionpage.setBackButton() //Back button validation
+    planselectionpage.setBackButton() 
    cy.wait(2000);
    cy.url().should('eq','http://169.61.105.110/medicareAdvantage_sandbox/plan-selection/plan-list/SUPPLEMENT');
  })
 
- it("Test 11: To verify the functionality of Cancel button on suppliment planlist page",()=>{
+ it("TC_PDP_SUPL_PLAN_98: To verify the functionality of Cancel button on suppliment planlist page",()=>{
     planselectionpage.setMedigapArrow()
     planselectionpage.setSelectPlanN()
     planselectionpage.setSupplementButtn();
@@ -212,22 +236,19 @@ it("Test9:To verify Other Benefits expands and collapse",()=>{
 
  })
 
- it("Test12: To verify functionality of Profile Button",()=>{
+ it("TC_PDP_MA_PLAN_121: To verify functionality of Profile Button",()=>{
     planselectionpage.setProfileButtn()
     cy.url().should("eq","http://169.61.105.110/medicareAdvantage_sandbox/home");
  })
 
- it("Test13: To verify functionality of Provider Button",()=>{
-    planselectionpage.setProviderButtn()
-    cy.url().should("eq","http://169.61.105.110/medicareAdvantage_sandbox/manage-providers");
- })
+ 
 
- it("Test14: To verify functionality of Prescription Button",()=>{
+ it("TC_PDP_MA_PLAN_122: To verify functionality of Prescription Button",()=>{
     planselectionpage.setPrescriptionButtn()
     cy.url().should("eq","http://169.61.105.110/medicareAdvantage_sandbox/manage-prescriptions");
  })
  
- it("Test15: To verify functionality of Pharmacy Button",()=>{
+ it("TC_PDP_MA_PLAN_123: To verify functionality of Pharmacy Button",()=>{
     planselectionpage.setPharmacyButtn()
     cy.url().should("eq","http://169.61.105.110/medicareAdvantage_sandbox/manage-pharmacies");
 
