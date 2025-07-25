7c79fa62078ebaafbf80bc71f0e3ee5b76484f87
Rani-Yelgalwar <rani.kondpak@gmail.com>
2025-07-25 17:20:31 -0600

    Update e2e.js
    updated with deleting comments

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -23,34 +23,10 @@ require('cypress-grep')()
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-/*beforeEach(function () {
-  let allowed = Cypress.env('allowedTests') || [];
-
-  // Normalize to array
-  if (typeof allowed === 'string') {
-    try {
-      const parsed = JSON.parse(allowed);
-      if (Array.isArray(parsed)) {
-        allowed = parsed;
-      } else {
-        allowed = allowed.split(',').map(s => s.trim());
-      }
-    } catch (e) {
-      allowed = allowed.split(',').map(s => s.trim());
-    }
-  }
-
-  const title = this.currentTest.title;
-  const testId = title.match(/(TC_[A-Za-z0-9_]+)/)?.[1];
-
-  if (testId && !allowed.includes(testId)) {
-    console.log(`⏩ Skipping test: ${testId}`);
-    this.skip();
-  }
-});*/
 
 
-/*beforeEach(function () {
+
+beforeEach(function () {
   let allowed = Cypress.env('allowedTests') || [];
 
   // Normalize to array
@@ -81,5 +57,5 @@ require('cypress-grep')()
     console.log(`⏩ Skipping test: ${testId}`);
     this.skip();
   }
-});*/
+});
 
